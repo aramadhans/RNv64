c4b11f758211935430764305568f3616e2832467
aramadhans <aramadhan@alamisharia.co.id>
2023-01-14 21:21:15 +0700

    init

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import {SafeAreaView, View, Text} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>Hello World</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default App;
