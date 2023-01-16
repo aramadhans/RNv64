4162961449e7a77811b1bd5cf21dde4c6c6bfa6b
aramadhans <aramadhan@alamisharia.co.id>
2023-01-16 10:36:29 +0700

    set react-native-web & hijra-os run on mobile

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
+import React from 'react';
 import {SafeAreaView, View, Text} from 'react-native';
+// import {Typography, AppBar, Button} from '@alami-group/hijra-os';
 
 const App = () => {
   return (
     <SafeAreaView>
+      {/* <AppBar title="Title + Icon" rightIcon="search" /> */}
       <View>
-        <Text>Hello World</Text>
+        <Text>Hello</Text>
+        {/* <Typography.Caption
+          bold
+          fontFamily="ibmplexmono"
+          color="foundation.accentVariant1">
+          Text
+        </Typography.Caption>
+
+        <Button label='Primary' variant='Primary' /> */}
       </View>
     </SafeAreaView>
   );
